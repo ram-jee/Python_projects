70d1bed48edc331780663d6307c09751a265be7a
unknown <Ram>
2020-07-09 21:49:19 +0530

    Functionality added

--- guess_number.py
+++ guess_number.py
@@ -1,24 +1,25 @@
+import random
 
-
-
-from random import randint
-
-
+print('Levels: 2-Easy, 5-Medium, 10-Challenge')
+level = int(input('Enter level: '))
+computer_val = random.randint(1,level)
+attempts = 1
+user_val = int(input('Enter choice: '))
 
 while True:
-  n = int(input('Enter pool size: '))
-  answer = randint(1,n)
-
-  guess = int(input('Enter your number: '))
-
-  if 0 < guess < n:
-    if guess == answer:
-      print(" \n             YOU WON :)")
+  if computer_val == user_val:
+    print('\nYou Won! Congratulations')
+    print(f'Expected: {computer_val} entererd: {user_val}')
+    print(f'Attempts: {attempts}')
+    break
+  else:
+    attempts += 1
+    print(f'\nYou Lost! \nExpected: {computer_val} entererd: {user_val}')
+    repeat = input('Want to try again, Yes or No?').lower()
+    if repeat == 'no':
+      print('Thank you for playing!')
       break
     else:
-      print(f'YOU LOST :( right number is {answer}')
-      chance = input(print('\nDo you want one more chance? Y or N?'))
-      if chance.lower() == 'y':
-        continue
-      else:
-        break
+      computer_val = random.randint(1,level)
+      user_val = int(input('Enter choice: '))
+
